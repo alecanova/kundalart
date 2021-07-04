8f5ee2e47ab633cffd9d22557fb059e0009186a3
Alessandro Canova <alessandrocanova14@gmail.com>
2021-07-04 21:32:57 +0200

    first trial

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>Kundalart</title>
+            <h1>Cos'è Kundalart?</h1>
+            <p>Borse sostenibili</p>
+        </main>
+    )
+}
+
+
+export default AboutPage
